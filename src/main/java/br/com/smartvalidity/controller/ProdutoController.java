8cab34971bb0fee02b90bf3c57bbe64b2189e13a
PietroDaniel <pietrofdaniel@gmail.com>
2025-01-30 16:12:31 -0300

    refactor: padronizar ProdutoController e ajustar injeção de dependência

--- src/main/java/br/com/smartvalidity/controller/ProdutoController.java
+++ src/main/java/br/com/smartvalidity/controller/ProdutoController.java
@@ -1,4 +1,48 @@
 package br.com.smartvalidity.controller;
 
+import br.com.smartvalidity.exception.SmartValidityException;
+import br.com.smartvalidity.model.entity.Produto;
+import br.com.smartvalidity.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
 public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> listarTodos() {
+        return ResponseEntity.ok(produtoService.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> buscarPorId(@PathVariable Integer id) throws SmartValidityException {
+        Produto produto = produtoService.buscarPorId(id);
+        return ResponseEntity.ok(produto);
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> criarProduto(@Valid @RequestBody Produto produto) throws SmartValidityException {
+        Produto novoProduto = produtoService.salvar(produto);
+        return ResponseEntity.status(201).body(novoProduto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Produto> atualizarProduto(@PathVariable Integer id, @Valid @RequestBody Produto produto) throws SmartValidityException {
+        Produto produtoAtualizado = produtoService.atualizar(id, produto);
+        return ResponseEntity.ok(produtoAtualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarProduto(@PathVariable Integer id) throws SmartValidityException {
+        produtoService.excluir(id);
+        return ResponseEntity.noContent().build();
+    }
 }
