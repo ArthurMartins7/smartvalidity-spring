62ad64c772d31fc513ba0c07b9f5c78346ec80df
PietroDaniel <pietrofdaniel@gmail.com>
2025-01-30 16:04:59 -0300

    refactor: ajustar ProdutoService para seguir padrão de injeção e exceções

--- src/main/java/br/com/smartvalidity/service/ProdutoService.java
+++ src/main/java/br/com/smartvalidity/service/ProdutoService.java
@@ -1,4 +1,48 @@
 package br.com.smartvalidity.service;
 
+import br.com.smartvalidity.exception.SmartValidityException;
+import br.com.smartvalidity.model.entity.Produto;
+import br.com.smartvalidity.model.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public Produto salvar(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public Produto buscarPorId(Integer id) throws SmartValidityException {
+        return produtoRepository.findById(id)
+                .orElseThrow(() -> new SmartValidityException("Produto não encontrado com o ID: " + id));
+    }
+
+    public List<Produto> listarTodos() {
+        return produtoRepository.findAll();
+    }
+
+    public Produto atualizar(Integer id, Produto produtoAtualizado) throws SmartValidityException {
+        Produto produto = buscarPorId(id);
+
+        produto.setCodigoBarras(produtoAtualizado.getCodigoBarras());
+        produto.setDescricao(produtoAtualizado.getDescricao());
+        produto.setMarca(produtoAtualizado.getMarca());
+        produto.setUnidadeMedida(produtoAtualizado.getUnidadeMedida());
+        produto.setQuantidade(produtoAtualizado.getQuantidade());
+        produto.setCategoria(produtoAtualizado.getCategoria());
+        produto.setFornecedores(produtoAtualizado.getFornecedores());
+
+        return produtoRepository.save(produto);
+    }
+
+    public void excluir(Integer id) throws SmartValidityException {
+        Produto produto = buscarPorId(id);
+        produtoRepository.delete(produto);
+    }
 }
