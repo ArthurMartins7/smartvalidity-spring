bc2d68c0dd1977e0c0592397aa32423fd754a77e
PietroDaniel <139817787+PietroDaniel@users.noreply.github.com>
2025-06-22 19:41:22 -0300

    add: Adiciona Suite que facilita o run de todos os testes

---
+++ src/test/java/br/com/smartvalidity/AllTestsSuite.java
@@ -0,0 +1,17 @@
+package br.com.smartvalidity;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+import org.springframework.test.context.ActiveProfiles;
+
+@Suite
+@SuiteDisplayName("SmartValidity - Suite Completa de Testes")
+@SelectPackages({
+    "br.com.smartvalidity",           // Inclui SmartvalidityApplicationTests
+    "br.com.smartvalidity.java.service"  // Inclui todos os *ServiceTest
+})
+@ActiveProfiles("test")
+public class AllTestsSuite {
+
+} 
